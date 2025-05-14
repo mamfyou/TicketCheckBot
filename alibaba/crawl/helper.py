f36cbcf7e2f1117299977829c2495f5ce7bf8e09
mamf <mamf20021381@gmail.com>
2025-05-14 13:53:15 +0330

    feat: expanding logic for mac os and linux

--- alibaba/crawl/helper.py
+++ alibaba/crawl/helper.py
@@ -1,7 +1,18 @@
-import winsound
+import os
+import platform
 
 
 class Notifier:
     @staticmethod
     def beep(frequency=700, duration=1000):
-        winsound.Beep(frequency, duration)
+        system = platform.system()
+
+        if system == 'Windows':
+            import winsound
+            winsound.Beep(frequency, duration)
+        elif system == 'Darwin':  # macOS
+            os.system(f"osascript -e 'beep {frequency}'")
+        elif system == 'Linux':
+            os.system(f"beep -f {frequency} -l {duration}")
+        else:
+            print("Unsupported platform")
