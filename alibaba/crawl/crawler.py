36dc6407a25c743e4d7e8ea304a88c2d7d778d7a
mamf <mamf20021381@gmail.com>
2025-05-14 13:43:39 +0330

    feat: expanding logic for mac os and linux

--- alibaba/crawl/crawler.py
+++ alibaba/crawl/crawler.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import time
 
 from scrapy.http import HtmlResponse
@@ -13,7 +14,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class AlibabaCrawler:
     def __init__(self, date_str: str, is_qom_to_teh: bool = True):
-        self.username = os.getlogin()
         self.date_str = date_str
         route = 'QUM-THR' if is_qom_to_teh else 'THR-QUM'
         self.BASE_URL = (f'https://www.alibaba.ir/train/{route}'
@@ -21,15 +21,39 @@ class AlibabaCrawler:
 
     def get_chrome_options(self):
         chrome_options = Options()
-        chrome_options.add_argument(f"user-data-dir=C:/Users/{self.username}/AppData/Local/Google/Chrome/User Data/Profile 1")
-        chrome_options.add_argument("profile-directory=Default")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        os_type = platform.system()
+
+        if os_type == "Windows":
+            user_profile = os.path.expandvars(r"%LOCALAPPDATA%\Google\Chrome\User Data")
+        elif os_type == "Darwin":
+            user_profile = os.path.expanduser("~/Library/Application Support/Google/Chrome")
+        elif os_type == "Linux":
+            user_profile = os.path.expanduser("~/.config/google-chrome")
+        else:
+            raise OSError("Unsupported OS")
+
+        if os.path.exists(user_profile):
+            chrome_options.add_argument(f"--user-data-dir={user_profile}")
+            chrome_options.add_argument("profile-directory=Default")
+
         return chrome_options
 
     def get_chrome_driver(self):
         driver_path = ChromeDriverManager().install()
-        os.system("taskkill /im chrome.exe /f")
+        self._close_chrome_process()
         driver = webdriver.Chrome(service=Service(driver_path), options=self.get_chrome_options())
         return driver
+
+    def _close_chrome_process(self):
+        os_type = platform.system()
+
+        if os_type == "Windows":
+            os.system("taskkill /im chrome.exe /f >nul 2>&1")
+        elif os_type in ["Linux", "Darwin"]:
+            os.system("pkill chrome > /dev/null 2>&1")
 
     def scrapy_tickets_page(self) -> tuple[HtmlResponse, webdriver.Chrome]:
         driver = self.get_chrome_driver()
